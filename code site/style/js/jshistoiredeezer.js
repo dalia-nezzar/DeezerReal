f492746e89908bf18bfa5281c4d706f20db08e9e
Calixteair <113100230+Calixteair@users.noreply.github.com>
2022-10-20 23:02:40 +0200

    LA page histoire
    pb de responsive sur le js

--- code site/style/js/jshistoiredeezer.js
+++ code site/style/js/jshistoiredeezer.js
@@ -8,16 +8,14 @@ const date = document.querySelector('d');
 const logoHeight = $(".logo1").height();
 const titres = $(".titre").height() / 2;
 const menuHeight = $(".id").height();
+const mainhead = $(".mainhead").height();
+const hglobal = $(".corps").height();
 
-$(function(){
-    var documentHeight = $(".corps").height();
-    $(".corps").css('height', (documentHeight + 1000));
-});
 
 const controller = new ScrollMagic.Controller();
 
 $(function(){
-    if (window.matchMedia("(min-width: 1000px)").matches) {
+    
   
        let isScrolling = false;
     
@@ -30,22 +28,25 @@ $(function(){
                requestAnimationFrame(render);
                var documentHeight = $(".corps").height();
                var scrollAmount = $(window).scrollTop();
-               var scrollAmountofcorps = $(window).scrollTop() - logoHeight;
-               $('progress').css('width', (documentHeight * 90 / 100));
-               $('progress').css('top', ((documentHeight * 95 / 100) / 2) - 20 + "px");
+               $('progress').css('width', (documentHeight * 95 / 100));
+               console.log (window.scrollY);
+
+               const decal = $("progress").width();
+               progressbar.max = decal;
+
+                $(function(){
+                    var documentHeight = $(".corps").height();
+                    $("progress").css('top', decal / 2 + 4 + "px");
+                });
 
                
 
                if (!isScrolling) return;
-                if(scrollAmount <= logoHeight){
-                    progressbar.value = 0;
-                    $('.date').css('color', "white");
-                }
-                else{
-                    console.log (scrollAmountofcorps);
-                    progressbar.value = scrollAmountofcorps * 100 / (documentHeight - logoHeight);
+
+
+                    progressbar.value = window.scrollY - 150;
                     $('.d1').css('color', "rgb(112, 254, 232)");
-                }
+
                 if(progressbar.value <= 22){
                     $('.d2').css('color', "white");
                 }
@@ -62,8 +63,11 @@ $(function(){
                 for(i = 0; i < allRonds.length; i++){
     
                     if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+
+
     
                         let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5});
+
     
                         let scene = new ScrollMagic.Scene({
                             triggerElement: allRonds[i],
@@ -80,14 +84,13 @@ $(function(){
     
             });
   
-            
-    
-    }
+    if (window.matchMedia("(min-width: 1000px)").matches) {
+
+     }
   
    else {
-    let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
-    .setTween(tween)
-    progressbar.value = 100
+
+    
    }
       
   });
